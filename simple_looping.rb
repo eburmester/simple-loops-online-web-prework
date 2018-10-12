38f0ed3aeecab13d0e682b556a3c708534f15a12
Elyse Burmester <burmesterelyse@gmail.com>
2018-10-12 19:54:13 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -36,7 +36,7 @@ end
 end
 
 def for_iterator(number_of_times)
-  for number in number_of_times do
+  for number in number_of_times 
    puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
