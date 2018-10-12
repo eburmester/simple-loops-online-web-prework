53dd84a940b2557e15435a4b3acf3398e590c1fc
Elyse Burmester <burmesterelyse@gmail.com>
2018-10-12 19:52:56 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -36,7 +36,7 @@ end
 end
 
 def for_iterator(number_of_times)
-  number_of_times = number_of_times
+  number_of_times = |number_of_times|
   for number in number_of_times do
    puts "Welcome to Flatiron School's Web Development Course!"
   end
